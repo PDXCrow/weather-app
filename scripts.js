24974def1843e8c4cfad22845bf58acda6df92b3
PDXCrow <piratehans@gmail.com>
2018-01-31 10:02:19 -0800

    restyling and pulling in more API data

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 
+//object to hold JSON data
+var weather = {};
+
+//global variables for lat and long
+var latitude = 0;
+var longitude = 0;
+
 //geolocate funtion to acquire lat and long
 function geoFindMe() {
   var output = document.getElementById("out");
@@ -9,10 +16,13 @@ function geoFindMe() {
   }
 
   function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    latitude  = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-    output.innerHTML = '<p>Latitude is ' + latitude + ' <br>Longitude is ' + longitude + ' </p>';
+    output.innerHTML = '<p>Latitude is ' + latitude + ' <br>Longitude is '
+    + longitude + ' </p>';
+
+    findWeather();
   }
 
   function error() {
@@ -22,20 +32,28 @@ function geoFindMe() {
   output.innerHTML = "<p>Locating...</p>";
 
   navigator.geolocation.getCurrentPosition(success, error);
+
+
 }
+
+
 
 //API call to get JSON data for weather info
 function findWeather () {
-  var requestURL = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139';//will need to add actual lat/long eventually
+  var requestURL = 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude;
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
   request.send();
+
   request.onload = function() {
-  var weather = request.response;
-  console.log(weather);
-  var newURL = weather.weather[0].icon;
-  console.log(newURL);
-  document.getElementById('icon').innerHTML = "<img src=" + newURL + ">";
+    //grab JSON data and place in weather
+    weather = request.response;
+    var newURL = weather.weather[0].icon; //get URL for icon image from JSON
+    document.getElementById('locationName').innerHTML = "Location: " + weather.name;
+    document.getElementById('conditions').innerHTML = "Current conditions: " + weather.weather[0].description;
+    document.getElementById('temp').innerHTML = "Temp: " + weather.main.temp + " C";
+    document.getElementById('icon').innerHTML = "<img src=" + newURL + ">";
+    document.getElementById('convert').innerHTML = "convert to F";
   }
 }
