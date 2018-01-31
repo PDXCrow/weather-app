cc1da968f1030e05cb030cc3dc9046bede2280b5
PDXCrow <piratehans@gmail.com>
2018-01-31 14:21:43 -0800

    cleaned up code and comments

--- scripts.js
+++ scripts.js
@@ -23,7 +23,8 @@ function geoFindMe() {
     latitude  = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    output.innerHTML = '<p id="latLong">Latitude is ' + latitude.toFixed(2) + ' <br>Longitude is '
+    output.innerHTML = '<p id="latLong">Latitude is ' + latitude.toFixed(2)
+    + ' <br>Longitude is '
     + longitude.toFixed(2) + ' </p>';
 
     //once geolocate is successful, call API function to get and display data
@@ -43,7 +44,8 @@ function geoFindMe() {
 
 function findWeather () {
   //API call to get JSON data for weather info
-  var requestURL = 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude;
+  var requestURL = 'https://fcc-weather-api.glitch.me/api/current?lat='
+  + latitude + '&lon=' + longitude;
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
@@ -52,14 +54,19 @@ function findWeather () {
   request.onload = function() {
     //grab JSON data and place in weather
     weather = request.response;
-    displayTemp = weather.main.temp.toFixed(1); //set global temp var so it can be unit converted as needed
+    //set global temp var so it can be unit converted as needed
+    displayTemp = weather.main.temp.toFixed(1);
     //get icon url, display loc, condition, temp, icon and conversion link
     var newURL = weather.weather[0].icon;
-    document.getElementById('locationName').innerHTML = "Location: " + weather.name;
+    document.getElementById('locationName').innerHTML = "Location: "
+    + weather.name;
     document.getElementById('conditions').innerHTML = "Current conditions: " + weather.weather[0].description;
-    document.getElementById('temp').innerHTML = "Temp: " + weather.main.temp.toFixed(1) + " C";
+    document.getElementById('temp').innerHTML = "Temp: " +
+    weather.main.temp.toFixed(1) + " C";
     document.getElementById('icon').innerHTML = "<img src=" + newURL + ">";
-    document.getElementById('convert').innerHTML = '<p id="convertLink" onclick="convertToF()">Change to Fahrenheit</p>';
+    document.getElementById('convert').innerHTML =
+    '<p id="convertLink" onclick="convertToF()">Change to Fahrenheit</p>';
+    //make div that allows lat/long change visible with results
     document.getElementById("modLoc").style.visibility= 'visible';
   }
 }
@@ -67,15 +74,19 @@ function findWeather () {
 function convertToF() {
   //convert global display var to Fahrenheit, reset link to convert back to C
   displayTemp = displayTemp * 1.8 + 32
-  document.getElementById('temp').innerHTML = "Temp: " + displayTemp.toFixed(1) + " F";
-  document.getElementById('convert').innerHTML = '<p id="convertLink" onclick="convertToC()">Change to Celsius</p>';
+  document.getElementById('temp').innerHTML = "Temp: " + displayTemp.toFixed(1)
+  + " F";
+  document.getElementById('convert').innerHTML =
+  '<p id="convertLink" onclick="convertToC()">Change to Celsius</p>';
 }
 
 function convertToC() {
   //convert global display var to Celsius, reset link to convert back to F
   displayTemp = (displayTemp - 32) / 1.8;
-  document.getElementById('temp').innerHTML = "Temp: " + displayTemp.toFixed(1) + " C";
-  document.getElementById('convert').innerHTML = '<p id="convertLink" onclick="convertToF()">Change to Fahrenheit</p>';
+  document.getElementById('temp').innerHTML = "Temp: " + displayTemp.toFixed(1)
+  + " C";
+  document.getElementById('convert').innerHTML =
+  '<p id="convertLink" onclick="convertToF()">Change to Fahrenheit</p>';
 }
 
 function changeLoc() {
@@ -84,6 +95,9 @@ function changeLoc() {
   longitude = document.getElementById("newLong").value;
   document.getElementById('newLat').value=''; //clear field
   document.getElementById('newLong').value=''; //clear field
-  document.getElementById('latLong').innerHTML = "Latitude is " + latitude + "<br>Longitude is " + longitude;
+  //display new lat/long numbers in results
+  document.getElementById('latLong').innerHTML = "Latitude is " + latitude
+  + "<br>Longitude is " + longitude;
+  //call function to provide new data based on revised lat/long
   findWeather();
 }
